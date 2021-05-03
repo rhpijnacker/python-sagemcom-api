db9877e5efd895591fe19956fac4d69e27c73c75
Mick Vleeshouwer <mick@imick.nl>
2021-05-03 02:12:08 +0200

    Improve function

--- sagemcom_api/client.py
+++ sagemcom_api/client.py
@@ -301,7 +301,7 @@ class SagemcomClient:
             except LoginTimeoutException:
                 pass
 
-        return None
+        raise LoginTimeoutException
 
     async def get_value_by_xpath(
         self, xpath: str, options: Optional[Dict] = {}
